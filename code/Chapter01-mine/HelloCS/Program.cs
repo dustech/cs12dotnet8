2ee7ea92cb09bd3e6cb87e3733f740d89bc81982
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-10-29 01:17:00 +0200

    Aggiornamento al Sun Oct 29 01:16:58 AM CEST 2023

---
+++ code/Chapter01-mine/HelloCS/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, C#!");
+
+new Cocco().Bongo(prodigioso: 9);
+
+public class Cocco
+{
+  public void Bongo(int prodigioso)
+  {
+    for (var i = 0; i < prodigioso; i++)
+    {
+      Console.WriteLine("bongo");
+    }
+
+  }
+}

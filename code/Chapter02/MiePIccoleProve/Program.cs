a74f026f01ad0a1cd17928ef2ccd05c29a673685
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-10-29 14:21:44 +0100

    Aggiornamento al Sun Oct 29 02:21:41 PM CET 2023

--- code/Chapter02/MiePIccoleProve/Program.cs
+++ code/Chapter02/MiePIccoleProve/Program.cs
@@ -7,15 +7,32 @@ namespace MiePiccoleProve
   {
     static void Main(string[] args)
     {
-      WriteLine("Hello, World!");
-      ProngoMizio prongo = new();
-      WriteLine($$"""{{prongo.DimmiTutto}}""");
+      // WriteLine("Hello, World!");
+      // ProngoMizio prongo = new();
+      // WriteLine($$"""{{prongo.DimmiTutto}}""");
+      ProngoMizio prongy = new();
+      prongy = null;
+      int bonzio = prongy?.Cannolo ?? 20;
+      int cano = prongy?.Cannolo ?? 35;
+      string myS = prongy?.DimmiTutto;
+      WriteLine(myS);
 
     }
   }
 
   public class ProngoMizio
   {
+    public ProngoMizio(string pinuccio)
+    {
+      if (string.IsNullOrEmpty(pinuccio))
+      {
+        throw new ArgumentException($"'{nameof(pinuccio)}' cannot be null or empty.", nameof(pinuccio));
+      }
+
+      Pinuccio = pinuccio;
+    }
+    public int Cannolo => 10;
     public string DimmiTutto { get => "Dimmi Tutto"; }
+    public string Pinuccio { get; }
   }
 }

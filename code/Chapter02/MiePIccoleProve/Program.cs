63b22aa1515c383e3b2dfdd3720a22c24e7b154a
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-10-29 10:17:07 +0100

    Aggiornamento al Sun Oct 29 10:17:05 AM CET 2023

---
+++ code/Chapter02/MiePIccoleProve/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace MiePiccoleProve
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, World!");
+      ProngoMizio prongo = new();
+      Console.WriteLine($$"""{{prongo.DimmiTutto}}""");
+
+    }
+  }
+
+  public class ProngoMizio
+  {
+    public string DimmiTutto { get => "Dimmi Tutto"; }
+  }
+}
